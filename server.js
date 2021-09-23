aa4ac41ff237feb9fd8003fc65cdd3e7616b005e
pierrebonjour <bonj.pierre@gmail.com>
2021-09-23 02:51:26 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -61,15 +61,17 @@ function sendListOfUploadedFiles(res){
 
 function sendUploadedFile(url, res){
   let file = path.join(__dirname, url);
-  fs.readFile(file, (err, content) => {
+  fs.readFile(file,'utf8', (err, content) => {
     if(err){
       res.writeHead(404, {'Content-Type': 'text'});
       res.write('File Not Found!');
       res.end();
     }else{
-      res.writeHead(200, {'Content-Type': 'application/octet-stream'});
-      res.write(content);
-      res.end();
+		var dataArray = data.split(/\r?\n/);  //Be careful if you are in a \r\n world...
+		
+		res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+		res.write(content);
+		res.end();
     }
   })
 }
@@ -79,8 +81,9 @@ function saveUploadedFile(req, res){
   console.log('saving uploaded file');
   let fileName = path.basename(req.url);
   let file = path.join(__dirname,'download', fileName)
-  req.pipe(fs.createWriteStream(file));
+  req.pipe(var fd = fs.createWriteStream(file));
   req.on('end', () => {
+	fs.close(fd.fd);
     res.writeHead(200, {'Content-Type': 'text'});
     res.write('uploaded succesfully to '+ file);
     res.end();
